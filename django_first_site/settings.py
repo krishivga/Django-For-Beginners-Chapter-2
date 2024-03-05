e4b725e4705e78df3889cd7fa609d7ed2555c316
Krishiv-Agarwal <krishiv.agarwal@cgpworldwide.com>
2024-03-05 12:53:25 +0530

    Set up urls and views, got helloworld running

--- django_first_site/settings.py
+++ django_first_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o0-(!frg9kf=i@q0z2yj%ixpz91fs3-er=6w#@hwm-p4))hd#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
